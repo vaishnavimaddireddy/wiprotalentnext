3686d913377689fc3a0ef7451ac9a6d0496e5043
vaishnavi <vaishnavi@Vaishnavi-HP>
2020-01-29 14:16:57 +0530

    java

---
+++ wipro/src/wipro/Employee.java
@@ -0,0 +1,21 @@
+package wipro;
+
+public class Employee {
+	private int empno;
+	private String empname;
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	
+	
+
+}
